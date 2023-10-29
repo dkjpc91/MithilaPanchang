63691379ddc6ef5e8c19132b805f5b93afb4179b
dkjpc91 <dkjpc91@gmail.com>
2023-10-29 23:46:57 +0530

    commit

---
+++ app/src/main/java/com/mithilakshar/mithilapanchang/MainActivity.java
@@ -0,0 +1,40 @@
+package com.mithilakshar.mithilapanchang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView splashTxt,logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        splashTxt=findViewById(R.id.splashTxt);
+        logo=findViewById(R.id.logo);
+
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.fade);
+        animation.setDuration(3000);
+        logo.startAnimation(animation);
+        splashTxt.startAnimation(animation);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i= new Intent(getApplicationContext(),home.class);
+                startActivity(i);
+                finish();
+            }
+        },4000);
+    }
+}
